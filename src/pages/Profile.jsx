44db98dcc5bf2f87b5664acb30b8a747f00b8fd1
Abakpa Joseph Elachi <elachi4c@yahoo.com>
2023-01-26 11:36:14 -0800

    update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import { db } from "../firebase.config";
 
 function Profile() {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -20,6 +21,10 @@ function Profile() {
     navigate("/");
   };
 
+  const onSubmit = () => {
+    console.log(123);
+  };
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -28,6 +33,28 @@ function Profile() {
           Logout
         </button>
       </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "Done" : "Change"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
