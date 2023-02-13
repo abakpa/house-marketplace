683277a711a70b4345695cfbee9fcc487198335f
Abakpa Joseph Elachi <elachi4c@yahoo.com>
2023-02-13 14:06:47 -0800

    display single page listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -75,7 +75,25 @@ function Listing() {
               ? `${listing.bedrooms} Bedrooms`
               : "1 Bedroom"}
           </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : "1 Bathroom"}
+          </li>
+          <li>{listing.parking && "Parking Sport"}</li>
+          <li>{listing.furnished && "Furnished"}</li>
         </ul>
+        <p className="listingLocationTitle">Location</p>
+        {/*MAP */}
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            className="primaryButton"
+          >
+            Contact Landlord
+          </Link>
+        )}
       </div>
     </main>
   );
